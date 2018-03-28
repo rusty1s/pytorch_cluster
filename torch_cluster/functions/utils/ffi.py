c20e6b58db923d4b5af8d4af925e88f105e1f515
rusty1s <matthias.fey@tu-dortmund.de>
2018-03-28 21:47:36 +0200

    rename

--- torch_cluster/functions/utils/ffi.py
+++ torch_cluster/functions/utils/ffi.py
@@ -6,7 +6,7 @@ def get_func(name, tensor):
     return getattr(ffi, 'cluster_{}{}'.format(name, cuda))
 
 
-def get_dynamic_func(name, tensor):
+def get_typed_func(name, tensor):
     typename = type(tensor).__name__.replace('Tensor', '')
     cuda = 'cuda_' if tensor.is_cuda else ''
     return getattr(ffi, 'cluster_{}_{}{}'.format(name, cuda, typename))
