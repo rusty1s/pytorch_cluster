2f0248cf847e22211fe460f13185639193962fb8
rusty1s <matthias.fey@tu-dortmund.de>
2018-03-30 16:38:14 +0200

    version bump

--- torch_cluster/__init__.py
+++ torch_cluster/__init__.py
@@ -1,7 +1,7 @@
 from .functions.serial import serial_cluster
 from .functions.grid import sparse_grid_cluster, dense_grid_cluster
 
-__version__ = '0.2.6'
+__version__ = '1.0.0'
 
 __all__ = [
     'serial_cluster', 'sparse_grid_cluster', 'dense_grid_cluster',
