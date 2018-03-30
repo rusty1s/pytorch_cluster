f9701cc2fe61515eca346a9fec58bdc799e247fd
rusty1s <matthias.fey@tu-dortmund.de>
2018-03-30 13:58:40 +0200

    typo

--- aten/THC/generic/THCGrid.cu
+++ aten/THC/generic/THCGrid.cu
@@ -1,5 +1,5 @@
 #ifndef THC_GENERIC_FILE
-#define THC_GENERIC_FILE "generic/THCGid.cu"
+#define THC_GENERIC_FILE "generic/THCGrid.cu"
 #else
 
 void THCGrid_(THCState *state, THCudaLongTensor *cluster, THCTensor *pos, THCTensor *size,
