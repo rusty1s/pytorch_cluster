6997baa9c82f01b92e91ebb987a536e5d142c155
rusty1s <matthias.fey@tu-dortmund.de>
2018-03-30 14:17:21 +0200

    bugfixes

--- aten/THC/THCGreedy.h
+++ aten/THC/THCGreedy.h
@@ -5,10 +5,18 @@
 
 #define THCGreedy_ TH_CONCAT_3(TH,CReal,Greedy)
 
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
 void THCGreedy(THCState *state, THCudaLongTensor *cluster, THCudaLongTensor *row,
                THCudaLongTensor *col, THCudaLongTensor *deg);
 
 #include "generic/THCGreedy.h"
 #include "THC/THCGenerateAllTypes.h"
+
+#ifdef __cplusplus
+}
+#endif  // __cplusplus
 
 #endif  // THC_GREEDY_INC
--- aten/THC/THCGrid.h
+++ aten/THC/THCGrid.h
@@ -5,7 +5,15 @@
 
 #define THCGrid_ TH_CONCAT_3(TH,CReal,Grid)
 
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
 #include "generic/THCGrid.h"
 #include "THC/THCGenerateAllTypes.h"
+
+#ifdef __cplusplus
+}
+#endif  // __cplusplus
 
 #endif  // THC_GRID_INC
