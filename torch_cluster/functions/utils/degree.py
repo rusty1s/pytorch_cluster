15aa4bb75a550b5fda246718a8889929f45cc0a6
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-07 19:38:20 +0200

    removed old code

--- torch_cluster/functions/utils/degree.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-
-def node_degree(index, num_nodes, out=None):
-    out = index.new(num_nodes) if out is None else out
-    zero = torch.zeros(num_nodes, out=out)
-    one = torch.ones(index.size(0), out=zero.new())
-    return zero.scatter_add_(0, index, one)
