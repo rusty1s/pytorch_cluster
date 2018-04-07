4576030c6268e7291f6d4b99ee1c5e863bd50c52
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-07 17:24:01 +0200

    first new grid impl

--- torch_cluster/grid.py
+++ torch_cluster/grid.py
@@ -1,6 +1,17 @@
 from .utils.ffi import grid
 
 
-def grid_cluster(pos, size, batch=None, start=None, end=None):
+def grid_cluster(pos, size, start=None, end=None):
     pos = pos.unsqueeze(-1) if pos.dim() == 1 else pos
-    grid(None, None, None, None)
+
+    start = pos.t().min(dim=1)[0] if start is None else start
+    end = pos.t().max(dim=1)[0] if end is None else end
+    pos, end = pos - start, end - start
+
+    size = size.type_as(pos)
+    count = (end / size).long() + 1
+
+    cluster = count.new(pos.size(0))
+    grid(cluster, pos, size, count)
+
+    return cluster
