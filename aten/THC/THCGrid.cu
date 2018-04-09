667363485d1d3a1122a672aa57d44f6a424a0362
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-09 15:13:44 +0200

    removed comment

--- aten/THC/THCGrid.cu
+++ aten/THC/THCGrid.cu
@@ -12,7 +12,7 @@ __global__ void gridKernel(int64_t *self, TensorInfo<T> posInfo, T *size,
     int64_t coef = 1, value = 0;
     for (ptrdiff_t d = 0; d < posInfo.size[1]; d += posInfo.stride[1]) {
       c = THCNumerics<T>::div(pos[d], size[d]);
-      c = ScalarConvert<int64_t, T>::to(ScalarConvert<T, int64_t>::to(c));  // floor.
+      c = ScalarConvert<int64_t, T>::to(ScalarConvert<T, int64_t>::to(c));
       value += coef * c;
       coef *= count[d];
     }
