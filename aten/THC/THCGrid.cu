5d199c866568d0a167722c928667aa3958326ab0
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-13 06:36:23 +0200

    half cuda bugfix

--- aten/THC/THCGrid.cu
+++ aten/THC/THCGrid.cu
@@ -8,12 +8,9 @@ __global__ void gridKernel(int64_t *self, TensorInfo<T> posInfo, T *size,
                            int64_t *count, ptrdiff_t nNodes) {
   KERNEL_LOOP(i, nNodes) {
     T *pos = posInfo.data + i * posInfo.stride[0];
-    T c;
     int64_t coef = 1, value = 0;
     for (ptrdiff_t d = 0; d < posInfo.size[1]; d += posInfo.stride[1]) {
-      c = THCNumerics<T>::div(pos[d], size[d]);
-      c = ScalarConvert<int64_t, T>::to(ScalarConvert<T, int64_t>::to(c));
-      value += coef * c;
+      value += coef * ScalarConvert<T, int64_t>::to(THCNumerics<T>::div(pos[d], size[d]));
       coef *= count[d];
     }
     self[i] = value;
