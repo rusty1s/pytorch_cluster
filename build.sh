2d253c11d978058818201b7519c1e048468fc2e4
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-30 07:49:56 +0200

    better compiler options

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ SRC_DIR=aten/THC
 BUILD_DIR=torch_cluster/_ext
 
 mkdir -p $BUILD_DIR
-$(which nvcc) -c -o "$BUILD_DIR/THC.so" "$SRC_DIR/THC.cu" -arch=sm_52 -Xcompiler -fPIC -shared "-I$TORCH/lib/include/TH" "-I$TORCH/lib/include" "-I$SRC_DIR"
+$(which nvcc) "-I$TORCH/lib/include" "-I$TORCH/lib/include/TH" "-I$TORCH/lib/include/THC" "-I$SRC_DIR" -c "$SRC_DIR/THC.cu" -o "$BUILD_DIR/THC.o" --compiler-options '-fPIC' -std=c++11
