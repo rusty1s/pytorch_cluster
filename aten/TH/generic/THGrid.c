b681c4b8ea727ab44c10c8b037ed0aa7d416ef95
Sebastian Pölsterl <sebp@k-d-w.org>
2018-08-09 15:59:40 +0200

    Fix build for pytorch 0.4.1
    THTensor is defined as an opaque data type,
    thus direct access to members is not possible anymore.
    Instead, use functions from C API to retrieve stride.
    
    Fixes #4

--- aten/TH/generic/THGrid.c
+++ aten/TH/generic/THGrid.c
@@ -6,16 +6,18 @@ void THTensor_(grid)(THLongTensor *self, THTensor *pos, THTensor *size, THLongTe
   int64_t *selfData = THLongTensor_data(self);
   real *posData = THTensor_(data)(pos);
   real *sizeData = THTensor_(data)(size);
+  int64_t posStride0 = THTensor_(stride)(pos, 0);
+  int64_t posStride1 = THTensor_(stride)(pos, 1);
   int64_t *countData = THLongTensor_data(count);
 
   ptrdiff_t n, d; int64_t coef, value;
   for (n = 0; n < THTensor_(size)(pos, 0); n++) {
     coef = 1; value = 0;
     for (d = 0; d < THTensor_(size)(pos, 1); d++) {
-      value += coef * (int64_t) (posData[d * pos->stride[1]] / sizeData[d]);
+      value += coef * (int64_t) (posData[d * posStride1] / sizeData[d]);
       coef *= countData[d];
     }
-    posData += pos->stride[0];
+    posData += posStride0;
     selfData[n] = value;
   }
 }
