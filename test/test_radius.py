2baa79fa5bc067cb105a36f5f05ad4e9547400ca
Jan Eric Lenssen <jel@nnaisense.com>
2019-02-12 10:07:22 +0100

    cpu radius version now max number of neighbors

--- test/test_radius.py
+++ test/test_radius.py
@@ -28,7 +28,6 @@ def test_radius(dtype, device):
     batch_y = tensor([0, 1], torch.long, device)
 
     out = radius(x, y, 2, batch_x, batch_y, max_num_neighbors=4)
-
     assert out.tolist() == [[0, 0, 0, 0, 1, 1], [0, 1, 2, 3, 5, 6]]
 
 
@@ -41,7 +40,7 @@ def test_radius_graph(dtype, device):
         [+1, -1],
     ], dtype, device)
 
-    row, col = radius_graph(x, r=2)
+    row, col = radius_graph(x, r=(2.0+1e-16))
 
     assert row.tolist() == [0, 0, 1, 1, 2, 2, 3, 3]
     assert col.tolist() == [1, 3, 0, 2, 1, 3, 0, 2]
