574bc32fe0d473e12789784765ac33e0ae67d913
rusty1s <matthias.fey@tu-dortmund.de>
2020-04-01 15:54:16 +0200

    clean up

--- torch_cluster/__init__.py
+++ torch_cluster/__init__.py
@@ -3,24 +3,14 @@ import os.path as osp
 
 import torch
 
-__version__ = '1.5.3'
-expected_torch_version = (1, 4)
+__version__ = '1.5.4'
 
-try:
-    for library in [
-            '_version', '_grid', '_graclus', '_fps', '_rw', '_sampler',
-            '_nearest', '_knn', '_radius'
-    ]:
-        torch.ops.load_library(importlib.machinery.PathFinder().find_spec(
-            library, [osp.dirname(__file__)]).origin)
-except OSError as e:
-    major, minor = [int(x) for x in torch.__version__.split('.')[:2]]
-    t_major, t_minor = expected_torch_version
-    if major != t_major or (major == t_major and minor != t_minor):
-        raise RuntimeError(
-            f'Expected PyTorch version {t_major}.{t_minor} but found '
-            f'version {major}.{minor}.')
-    raise OSError(e)
+for library in [
+        '_version', '_grid', '_graclus', '_fps', '_rw', '_sampler', '_nearest',
+        '_knn', '_radius'
+]:
+    torch.ops.load_library(importlib.machinery.PathFinder().find_spec(
+        library, [osp.dirname(__file__)]).origin)
 
 if torch.version.cuda is not None:  # pragma: no cover
     cuda_version = torch.ops.torch_cluster.cuda_version()
