151c1d32346ef36c36ae3c2a5b69aeab90dbb693
rusty1s <matthias.fey@tu-dortmund.de>
2020-10-29 16:04:15 +0100

    delete kind

--- script/torch.sh
+++ script/torch.sh
@@ -7,16 +7,11 @@ if [ "${TRAVIS_OS_NAME}" = "windows" ]; then
   sed -i.bak -e 's/return \*(this->value)/return \*((type\*)this->value)/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/pybind11/cast.h
 fi
 
-# if [ "${TRAVIS_OS_NAME}" = "windows" ] && [ "${TORCH_VERSION}" = "1.7.0" ]; then
-#   echo "Fix nvcc for PyTorch 1.7.0"
-#   sed -i.bak -e 's/constexpr Symbol Kind = ::c10::prim::profile;/const Symbol Kind;/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/ir/ir.h
-#   sed -i.bak '1345a\
-# const Symbol ProfileOp::Kind = ::c10::prim::profile;' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/ir/ir.h
-#   sed -i.bak -e 's/constexpr Symbol Kind = ::c10::prim::profile_optional;/const Symbol Kind;/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/ir/ir.h
-#   sed -i.bak '1368a\
-# const Symbol ProfileOptionalOp::Kind = ::c10::prim::profile_optional;' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/ir/ir.h
+if [ "${TRAVIS_OS_NAME}" = "windows" ] && [ "${TORCH_VERSION}" = "1.7.0" ]; then
+  echo "Fix nvcc for PyTorch 1.7.0"
+  sed -i.bak '/static constexpr Symbol Kind/d' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/ir/ir.h
 
-#   echo "-------------------------"
-#   sed -n '1327,1375p' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/ir/ir.h
-#   echo "-------------------------"
-# fi
+  echo "-------------------------"
+  sed -n '1327,1375p' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/ir/ir.h
+  echo "-------------------------"
+fi
