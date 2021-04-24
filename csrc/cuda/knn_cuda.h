8c87af25dbec765ae06f27a09dc1e6f5a4e6c2a3
Duc <adnguyen@yonsei.ac.kr>
2021-04-24 16:17:44 +0900

    an implementation of knn cuda

--- csrc/cuda/knn_cuda.h
+++ csrc/cuda/knn_cuda.h
@@ -4,5 +4,5 @@
 
 torch::Tensor knn_cuda(torch::Tensor x, torch::Tensor y,
                        torch::optional<torch::Tensor> ptr_x,
-                       torch::optional<torch::Tensor> ptr_y, int64_t k,
+                       torch::optional<torch::Tensor> batch_y, int64_t k,
                        bool cosine);
