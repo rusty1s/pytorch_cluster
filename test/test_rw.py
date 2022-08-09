c4154c8ac772ee5e8c6bb3b36e7d736bec04e9a3
Piotr Bielak <piotrbielak2@gmail.com>
2022-08-09 16:32:09 +0200

    Implement weighted random walks
    This commit implements weighted biased random walks as in the original
    Node2vec paper. In particular, it adds a new parameter to the `random_walk`
    function, i.e., `edge_weight`, which allows passing edge weights to the
    underlying random walk generation procedure. If edge weights are set,
    the function normalizes them by the node degree and converts the weights
    into CDFs over given nodes (needed by the rejection sampling method).
    The implementation of the new rejection sampling method is based on [1].
    
    [1] https://github.com/louisabraham/fastnode2vec/blob/master/fastnode2vec/graph.py#L69
    
    * Update `random_walk` API
    * Implement weighted rejection sampling on CPU
    * Implement weighted random walk for GPU (CUDA)
    * Compute CDFs using C++/CUDA
    * Add tests for weighted random walks

--- test/test_rw.py
+++ test/test_rw.py
@@ -77,3 +77,44 @@ def test_rw_small_with_edge_indices(device):
         [1, 0, 1, 0],
         [-1, -1, -1, -1],
     ]
+
+
+@pytest.mark.parametrize('device', devices)
+def test_rw_large_with_edge_weights(device):
+    row = tensor([0, 1, 1, 1, 2, 2, 3, 3, 4, 4], torch.long, device)
+    col = tensor([1, 0, 2, 3, 1, 4, 1, 4, 2, 3], torch.long, device)
+    start = tensor([0, 1, 2, 3, 4], torch.long, device)
+    edge_weight = tensor([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], torch.float, device)
+    walk_length = 10
+
+    out = random_walk(
+        row, col, start, walk_length,
+        edge_weight=edge_weight,
+    )
+    assert out[:, 0].tolist() == start.tolist()
+
+    for n in range(start.size(0)):
+        cur = start[n].item()
+        for i in range(1, walk_length):
+            assert out[n, i].item() in col[row == cur].tolist()
+            cur = out[n, i].item()
+
+
+@pytest.mark.parametrize('device', devices)
+def test_rw_small_with_edge_weights(device):
+    row = tensor([0, 1], torch.long, device)
+    col = tensor([1, 0], torch.long, device)
+    start = tensor([0, 1, 2], torch.long, device)
+    edge_weight = tensor([1, 1], torch.float, device)
+    walk_length = 4
+
+    out = random_walk(
+        row, col, start, walk_length,
+        num_nodes=3,
+        edge_weight=edge_weight,
+    )
+    assert out.tolist() == [
+        [0, 1, 0, 1, 0],
+        [1, 0, 1, 0, 1],
+        [2, 2, 2, 2, 2],
+    ]
