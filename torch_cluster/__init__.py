690b06f1562ff71be7370e72caadb282cf4127a6
rusty1s <matthias.fey@tu-dortmund.de>
2023-03-16 13:43:38 +0000

    update

--- torch_cluster/__init__.py
+++ torch_cluster/__init__.py
@@ -3,7 +3,7 @@ import os.path as osp
 
 import torch
 
-__version__ = '1.6.0'
+__version__ = '1.6.1'
 
 for library in [
         '_version', '_grid', '_graclus', '_fps', '_rw', '_sampler', '_nearest',
