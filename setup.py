84657d3c8b73f773fee24c6efee3f4a7ea91ebb2
rusty1s <matthias.fey@tu-dortmund.de>
2026-03-09 10:03:24 +0000

    update

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ def get_extensions():
             nvcc_flags += ['-O2']
             nvcc_flags += ['-DTORCH_INDUCTOR_CPP_WRAPPER']
             if sys.platform == 'win32':
-                nvcc_flags += ['-D_WIN32=1']
+                nvcc_flags += ['-Xcompiler', '/permissive-']
             extra_compile_args['nvcc'] = nvcc_flags
 
             if torch.version.hip:
